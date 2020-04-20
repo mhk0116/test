ac963ac15ac409ab3b19b1c686d7baacccb165a9
Minhwan Kim <mhk0116@gmail.com>
2020-04-21 06:24:53 +0900

    Updates

---
+++ precache-manifest.cf9f568c459c1b75bcd8e90e2a22312c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f8c4d520e9d106bc9a7d81d0ef87cc",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "ab812b2e2607a67f20df",
+    "url": "/test/static/css/main.83ca4d70.chunk.css"
+  },
+  {
+    "revision": "dbcb8eed2f9729f5a0e1",
+    "url": "/test/static/js/2.4a4654ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.4a4654ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab812b2e2607a67f20df",
+    "url": "/test/static/js/main.1d3cb124.chunk.js"
+  },
+  {
+    "revision": "cc0e2c9cc93fc8222211",
+    "url": "/test/static/js/runtime-main.446871e8.js"
+  }
+]);
